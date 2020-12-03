0f74a170112741b1336df01f51f762adec6163e4
Jinyan Liu <jinyan.liu@bontouch.com>
2020-12-03 10:41:36 +0100

    Finish day 3

--- aoc2020/src/main/kotlin/D03.kt
+++ aoc2020/src/main/kotlin/D03.kt
@@ -7,12 +7,12 @@ class Day03 {
         val inputsList = getInputs()
         val lines = inputsList.size
         val maxX = lines * 3
-        val singleMaxX = inputsList.get(0).length
+        val singleMaxX = inputsList[0].length
         val repetition = maxX / singleMaxX + 1
 
         var counter = 0
         inputsList.forEachIndexed { index, s ->
-            val toVerify = s.repeat(repetition).get(index * 3).toString()
+            val toVerify = s.repeat(repetition)[index * 3].toString()
             if (toVerify == "#") {
                 counter += 1
             }
@@ -20,44 +20,28 @@ class Day03 {
         return counter
     }
 
-    fun getSolution2() {
-        val slope11 = getSlope(1, 1)
-        val slope31 = getSlope(3, 1)
-        val slope51 = getSlope(5, 1)
-        val slope71 = getSlope(7, 1)
-        val slope12 = getSlope(1, 2)
+    fun getSolution2(): Long {
+        val slope11 = getSlopeBy(1, 1)
+        val slope31 = getSlopeBy(3, 1)
+        val slope51 = getSlopeBy(5, 1)
+        val slope71 = getSlopeBy(7, 1)
+        val slope12 = getSlopeBy(1, 2)
+        return slope11 * slope31 * slope51 * slope71 * slope12
     }
 
-    private fun getSlope(right: Int, down: Int): Long {
-        val inputsList = IoHelper.getLines("d03.in")
+    private fun getSlopeBy(right: Int, down: Int): Long {
+        val ratio = right.toDouble() / down
+        val inputsList = getInputs()
         val lines = inputsList.size
-        val maxX = lines * (right / down)
-        val singleMaxX = inputsList.get(0).length
-        val repetition = maxX / singleMaxX + 1
-
-        var counter:Long = 0
-        inputsList.forEachIndexed { index, s ->
-            val toVerify = s.repeat(repetition).get(index * (right / down)).toString()
-            if (toVerify == "#") {
-                counter += 1
-            }
-        }
-        return counter
-    }
-
-    private fun getSlopeDown2(right: Int, down: Int): Long {
-        val inputsList = IoHelper.getLines("d03.in")
-        val lines = inputsList.size
-        val maxX = lines * 0.5
-        val singleMaxX = inputsList.get(0).length
+        val maxX = lines * ratio
+        val singleMaxX = inputsList[0].length
         val repetition = maxX.toInt() / singleMaxX + 1
 
-        var counter:Long = 0
+        var counter: Long = 0
         inputsList.forEachIndexed { index, s ->
-            if (index % 2 == 0) {
-                val correctIndex = index/2
-                val toVerify = s.repeat(repetition).get(correctIndex).toString()
-                println(toVerify+" "+index.toString())
+            if (index % down == 0) {
+                val countedIndex = (index.toDouble() * ratio).toInt()
+                val toVerify = s.repeat(repetition)[countedIndex].toString()
                 if (toVerify == "#") {
                     counter += 1
                 }
@@ -66,27 +50,9 @@ class Day03 {
         }
         return counter
     }
-
-    fun testD03(): Long {
-        val slope11 = getSlope(1, 1)
-        val slope31 = getSlope(3, 1)
-        val slope51 = getSlope(5, 1)
-        val slope71 = getSlope(7, 1)
-        val slope12 = getSlopeDown2(1, 2)
-        println(slope11)
-        println(slope31)
-        println(slope51)
-        println(slope71)
-        println(slope12)
-        println(slope11 * slope31 * slope51 * slope71 * slope12)
-        return slope11 * slope31 * slope51 * slope71 * slope12
-    }
 }
 
 fun main() {
-/*    println(Day03().getSolution1())
-    println(Day03().getSolution2())*/
-
-    println(Day03().testD03())
-
+    println(Day03().getSolution1())
+    println(Day03().getSolution2())
 }
---
+++ aoc2020/src/main/kotlin/D04.kt
@@ -0,0 +1,16 @@
+import utils.IoHelper
+
+class Day04 {
+    private fun getInputs() = IoHelper.getLines("d04.in")
+
+    fun getSolution1() {
+    }
+
+    fun getSolution2() {
+    }
+}
+
+fun main() {
+    println(Day04().getSolution1())
+    println(Day04().getSolution2())
+}
