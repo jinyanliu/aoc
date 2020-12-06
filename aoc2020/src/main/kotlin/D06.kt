6f4a338d8bf86422db416a2e7de225b42deb97c1
Jinyan Liu <jinyan.liu@bontouch.com>
2020-12-06 12:26:10 +0100

    Finish day 6

--- aoc2020/src/main/kotlin/D06.kt
+++ aoc2020/src/main/kotlin/D06.kt
@@ -1,12 +1,29 @@
 import utils.IoHelper
 
 class Day06 {
-    private fun getInputs() = IoHelper.getLines("d06.in")
+    private fun getInputs() = IoHelper.getSections("d06.in")
 
-    fun getSolution1() {
-    }
+    fun getSolution1() = getInputs().map { it.replace("\n", "").toList().distinct().size }.sum()
 
-    fun getSolution2() {
+    fun getSolution2() = getInputs().map { validCount(it) }.sum()
+
+    private fun validCount(input: String): Int {
+        var validCount = 0
+        val toVerifyList = input.lines()
+        val repeatationTimes = toVerifyList.size
+        val toVerifyFirstKey = toVerifyList.get(0)
+        for (char in toVerifyFirstKey) {
+            var valid = true
+            for (i in 0..repeatationTimes - 1) {
+                if (!toVerifyList.get(i).contains(char)) {
+                    valid = false
+                }
+            }
+            if (valid) {
+                validCount = validCount + 1
+            }
+        }
+        return validCount
     }
 }
 
