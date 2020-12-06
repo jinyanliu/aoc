0d3b1990ad10f76cbfbccf6c95d498912c901692
Jinyan Liu <jinyan.liu@bontouch.com>
2020-12-06 13:16:51 +0100

    Refactor day 6

--- aoc2020/src/main/kotlin/D06.kt
+++ aoc2020/src/main/kotlin/D06.kt
@@ -6,7 +6,7 @@ class Day06 {
     fun getSolution1() = getInputs().map { it.replace("\n", "").toList().distinct().size }.sum()
 
     fun getSolution2() = getInputs().map { it.lines().sortedBy { line -> line.length } }
-        .map { it[0].count { char -> it.all { toVerify -> toVerify.contains(char) } } }
+        .map { sortedList -> sortedList[0].count { char -> sortedList.all { item -> item.contains(char) } } }
         .sum()
 }
 
