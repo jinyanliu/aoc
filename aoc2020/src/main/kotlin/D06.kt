5ad028d08d1d5e5f4da472a7e66a4a593768bf6b
Jinyan Liu <jinyan.liu@bontouch.com>
2020-12-06 12:53:27 +0100

    Refactor day 6

--- aoc2020/src/main/kotlin/D06.kt
+++ aoc2020/src/main/kotlin/D06.kt
@@ -5,10 +5,9 @@ class Day06 {
 
     fun getSolution1() = getInputs().map { it.replace("\n", "").toList().distinct().size }.sum()
 
-    fun getSolution2() = getInputs().map { validCount(it) }.sum()
+    fun getSolution2() = getInputs().map { it.lines() }.map { charInEveryItemCount(it) }.sum()
 
-    private fun validCount(input: String): Int {
-        val toVerifyList = input.lines()
+    private fun charInEveryItemCount(toVerifyList: List<String>): Int {
         val toVerifyShortestItem = toVerifyList.sortedBy { it.length }[0]
         return toVerifyShortestItem.count { toVerifyList.all { toVerify -> toVerify.contains(it) } }
     }
