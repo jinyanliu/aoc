04df6f44e744052a1d6271c64c242a89496cc383
Jinyan Liu <jinyan.liu@bontouch.com>
2020-12-07 18:06:55 +0100

    Finish day 7

--- aoc2020/src/main/kotlin/D07.kt
+++ aoc2020/src/main/kotlin/D07.kt
@@ -1,9 +1,34 @@
 import utils.IoHelper
 
 class Day07 {
-    private fun getInputs() = IoHelper.getLines("d07Test2.in")
+    private fun getInputs() = IoHelper.getLines("d07.in")
 
-    fun getSolution1() {
+    fun getSolution2() :Int{
+        val listOfBags = getInputs().map { generateBagSpecification2(it) }
+        val resultBags = arrayListOf(1 to "shinygold")
+        var keys = arrayListOf(1 to "shinygold")
+        while (keys.size > 0) {
+            val newKeys = arrayListOf<Pair<Int, String>>()
+            for (key in keys) {
+                for (bag in listOfBags) {
+                    if (bag.bag == key.second) {
+
+                        repeat(key.first) {
+                            resultBags.addAll(bag.contains)
+                            newKeys.addAll(bag.contains)
+                        }
+
+                    }
+                }
+            }
+            keys = newKeys
+        }
+
+        return resultBags.sumBy { it.first } - 1
+
+    }
+
+    fun getSolution1():Int {
         val listOfBags = getInputs().map { generateBagSpecification1(it) }
         val resultBags = arrayListOf<String>()
         var keys = arrayListOf("shinygold")
@@ -12,18 +37,15 @@ class Day07 {
             for (key in keys) {
                 for (bag in listOfBags) {
                     if (bag.contains.contains(key)) {
-                        println(bag.bag)
                         resultBags.add(bag.bag)
                         newKeys.add(bag.bag)
                     }
                 }
             }
             keys = newKeys
-            println()
         }
 
-        println(resultBags)
-        println(resultBags.distinct().size)
+        return resultBags.distinct().size
 
 
     }
@@ -33,7 +55,6 @@ class Day07 {
         val listOfBags =
             line.split("contain").get(1).trim().split(",").map { it.trim().split(" ")[1] + it.trim().split(" ")[2] }
         val BagSpecification = BagSpecification1(bag, listOfBags)
-        println(BagSpecification)
         return BagSpecification
     }
 
@@ -49,39 +70,13 @@ class Day07 {
     fun String.getBagNumber() = this.trim().split(" ")[0].toIntOrNull() ?: 0
 
 
-    fun getSolution2() {
-        val listOfBags = getInputs().map { generateBagSpecification2(it) }
-        val resultBags = arrayListOf(1 to "shinygold")
-        var keys = arrayListOf(1 to "shinygold")
-        while (keys.size > 0) {
-            val newKeys = arrayListOf<Pair<Int, String>>()
-            for (key in keys) {
-                for (bag in listOfBags) {
-                    if (bag.bag == key.second) {
-                        println(bag)
 
-                        repeat(resultBags.filter { it.second == bag.bag }.sumBy { it.first }) {
-                            resultBags.addAll(bag.contains)
-                        }
-                        println(resultBags.filter { it.first != 0 })
-                        newKeys.addAll(bag.contains)
-                    }
-                }
-            }
-            keys = newKeys
-            println()
-        }
-
-        println(resultBags.filter { it.first != 0 })
-        println(resultBags.sumBy { it.first } - 1)
-
-    }
 }
 
 data class BagSpecification1(val bag: String, val contains: List<String>)
 data class BagSpecification2(val bag: String, val contains: List<Pair<Int, String>>)
 
 fun main() {
-    //println(Day07().getSolution1())
+    println(Day07().getSolution1())
     println(Day07().getSolution2())
 }
