959fc1031fc248e16132a247c3c5effc8d6aeba3
Jinyan Liu <jinyan.liu@bontouch.com>
2020-12-08 11:40:15 +0100

    Refactor day 08

--- aoc2020/src/main/kotlin/D08.kt
+++ aoc2020/src/main/kotlin/D08.kt
@@ -3,66 +3,38 @@ import utils.IoHelper
 class Day08 {
     private fun getInputs() = IoHelper.getLines("d08.in")
 
-    fun getSolution1() {
-        val mapLines =
-            getInputs().mapIndexed { index, s -> index to (s.split(" ")[0] to s.split(" ")[1].toIntOrNull()) }.toMap()
-                .toMutableMap()
+    val mapLines = getInputs()
+        .mapIndexed { index, s -> index to (s.split(" ")[0] to s.split(" ")[1].toIntOrNull()) }
+        .toMap()
+        .toMutableMap()
 
-        var i = 0
-        var currentValue = 0
-        while (mapLines[i]?.first != "visited") {
-            val currentMapI = mapLines[i]
-            when (currentMapI?.first) {
-                "nop" -> {
-                    mapLines[i] = "visited" to 0
-                    println(currentMapI)
-                    i += 1
-                }
-                "acc" -> {
-                    mapLines[i] = "visited" to 0
-                    println(currentMapI)
-                    currentValue += currentMapI.second ?: 0
-                    i += 1
-                    println(currentValue)
-                }
-                else -> {
-                    mapLines[i] = "visited" to 0
-                    println(currentMapI)
-                    i += currentMapI?.second ?: 0
-                }
-            }
-        }
-        println(currentValue)
+    fun getSolution1():Int {
+        return getCurrentValue(mapLines)
     }
 
     fun getSolution2() {
-        val mapLines =
-            getInputs().mapIndexed { index, s -> index to (s.split(" ")[0] to s.split(" ")[1].toIntOrNull()) }.toMap()
-                .toMutableMap()
-
-
-
-        for (line in mapLines){
-            when(line.value.first){
-                "nop"->{
+        for (line in mapLines) {
+            when (line.value.first) {
+                "nop" -> {
                     val newInstructions = mutableMapOf<Int, Pair<String, Int?>>()
                     newInstructions.putAll(mapLines.toMap())
                     newInstructions[line.key] = "jmp" to line.value.second
                     getCurrentValue(newInstructions)
                 }
-                "jmp"->{
+                "jmp" -> {
                     val newInstructions = mutableMapOf<Int, Pair<String, Int?>>()
                     newInstructions.putAll(mapLines.toMap())
                     newInstructions[line.key] = "nop" to line.value.second
                     getCurrentValue(newInstructions)
                 }
-                "acc"->{}
+                "acc" -> {
+                }
             }
         }
 
     }
 
-    private fun getCurrentValue(mapLines: MutableMap<Int, Pair<String, Int?>>) {
+    private fun getCurrentValue(mapLines: MutableMap<Int, Pair<String, Int?>>): Int {
         var i = 0
         var currentValue = 0
         while (mapLines[i]?.first != "visited") {
@@ -70,32 +42,28 @@ class Day08 {
             when (currentMapI?.first) {
                 "nop" -> {
                     mapLines[i] = "visited" to 0
-                    println(currentMapI)
                     i += 1
                 }
                 "acc" -> {
                     mapLines[i] = "visited" to 0
-                    println(currentMapI)
                     currentValue += currentMapI.second ?: 0
                     i += 1
-                    println(currentValue)
                 }
                 "jmp" -> {
                     mapLines[i] = "visited" to 0
-                    println(currentMapI)
-                    i += currentMapI?.second ?: 0
+                    i += currentMapI.second ?: 0
                 }
-                null ->{
+                null -> {
                     mapLines[i] = "visited" to 0
-                    println("null"+currentValue)
+                    println("null$currentValue")
                 }
             }
         }
-        println(currentValue)
+        return currentValue
     }
 }
 
 fun main() {
-    //println(Day08().getSolution1())
+    println(Day08().getSolution1())
     println(Day08().getSolution2())
 }
