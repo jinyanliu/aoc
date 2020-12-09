c8043fb6e731f80dd5de48782799c17ce3e6c68c
Jinyan Liu <jinyan.liu@bontouch.com>
2020-12-09 21:31:19 +0100

    Prepare day 10

--- aoc2020/src/main/kotlin/D09.kt
+++ aoc2020/src/main/kotlin/D09.kt
@@ -3,50 +3,41 @@ import utils.IoHelper
 class Day09 {
     private fun getInputs() = IoHelper.getLongs("d09.in")
 
-    //177777905
-    fun getSolution1() :Long{
-        val mapIntegerList = getInputs().mapIndexed { index, integer -> index to integer }.toMap()
-        for (integ in mapIntegerList) {
-            if (integ.key > 25) {
-                val resultList = arrayListOf<Long>()
+    private val mapLongList = getInputs().mapIndexed { index, long -> index to long }.toMap()
 
-                val indexKey = integ.key
-                val indexRange = (indexKey - 25..indexKey - 1).toList()
-                val list1 = indexRange.map { mapIntegerList.get(it) }
-                val list2 = indexRange.map { mapIntegerList.get(it) }.toMutableList()
-                list1.forEach { i ->
-                    list2.forEach { j ->
-                        if ((i!! + j!!) != (i + i)) {
+    fun getSolution1(): Long {
+        for (mapLong in mapLongList) {
+            if (mapLong.key >= 25) {
+                val resultList = arrayListOf<Long>()
+                val indexRange = (mapLong.key - 25 until mapLong.key)
+                val targetList = indexRange.map { mapLongList[it] ?: error("error 1") }
+                targetList.forEach { i ->
+                    targetList.forEach { j ->
+                        if ((i + j) != (i + i)) {
                             resultList.add(i + j)
                         }
                     }
                 }
-
-                if (!resultList.contains(integ.value)) {
-                    return (integ.value)
+                if (!resultList.contains(mapLong.value)) {
+                    return (mapLong.value)
                 }
             }
-
         }
         return 0
     }
 
-    fun getSolution2():Long {
-        val mapIntegerList = getInputs().mapIndexed { index, integer -> index to integer }.toMap()
-        for(integ in mapIntegerList){
-            val currentIntegKey = integ.key
-            var currentResult:Long = integ.value
-            var currentStep= 1
-            while(currentResult<177777905L){
-                val currentEndKey = currentIntegKey+currentStep
-
-                    currentResult += mapIntegerList[currentEndKey]!!
-                    currentStep +=1
-                    if(currentResult == 177777905L){
-                        val a = (currentIntegKey..currentEndKey).map { mapIntegerList.get(it)!! }.max()!!
-                        val b = (currentIntegKey..currentEndKey).map { mapIntegerList.get(it)!! }.min()!!
-                        return a+b
+    fun getSolution2(): Long {
+        for (mapLong in mapLongList) {
+            var currentResult: Long = mapLong.value
+            var currentStep = 1
+            while (currentResult < 177777905L) {
+                val currentEndKey = mapLong.key + currentStep
+                currentResult += mapLongList[currentEndKey] ?: error("error 2")
+                if (currentResult == 177777905L) {
+                    val resultValueList = (mapLong.key..currentEndKey).map { mapLongList[it] ?: error("") }
+                    return (resultValueList.max() ?: 0) + (resultValueList.min() ?: 0)
                 }
+                currentStep += 1
             }
         }
         return 0
---
+++ aoc2020/src/main/kotlin/D10.kt
@@ -0,0 +1,16 @@
+import utils.IoHelper
+
+class Day10 {
+    private fun getInputs() = IoHelper.getLines("d10.in")
+
+    fun getSolution1() {
+    }
+
+    fun getSolution2() {
+    }
+}
+
+fun main() {
+    println(Day10().getSolution1())
+    println(Day10().getSolution2())
+}
