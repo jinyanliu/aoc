fc7eb103c69dd159ec93eb8237eb0a9d126b0c64
Jinyan Liu <jinyan.liu@bontouch.com>
2020-12-10 17:47:01 +0100

    Finish day 10

--- aoc2020/src/main/kotlin/D10.kt
+++ aoc2020/src/main/kotlin/D10.kt
@@ -1,6 +1,4 @@
 import utils.IoHelper
-import kotlin.math.log2
-import kotlin.math.pow
 
 class Day10 {
     private val inputs = IoHelper.getInts("d10.in")
@@ -34,177 +32,111 @@ class Day10 {
         newList.add(deviceRate)
         newList.sort()
 
-        println(newList)
+        println("newList=" + newList.toString())
 
-        //val testList = arrayListOf(0, 1, 4, 5, 6, 7, 10, 11, 12)
-        val testList = newList
-        //val testList = arrayListOf(0, 1, 4, 5, 6)
-        //filterStraightforwardLists(testList)
+        val liiist = arrayListOf(
+            0,
+            1,
+            2,
+            3,
+            4,
+            7,
+            8,
+            11,
+            12,
+            13,
+            16,
+            17,
+            18,
+            19,
+            20,
+            23,
+            26,
+            27,
+            28,
+            31,
+            34,
+            35,
+            36,
+            37,
+            40,
+            41,
+            42,
+            43,
+            46,
+            49
+        )
+        val originList = arrayListOf(
+            0,
+            1,
+            2,
+            3,
+            4,
+            7,
+            8,
+            11,
+            12,
+            13,
+            16,
+            17,
+            18,
+            19,
+            20,
+            23,
+            26,
+            27,
+            28,
+            31,
+            34,
+            35,
+            36,
+            37,
+            40,
+            41,
+            42,
+            43,
+            46,
+            49
+        )
 
-        val resultBags = arrayListOf<Int>()
+        val result: Long = 3136L * 7 * 7 * 4 * 7 * 7 * 2 * 7 * 7 * 4 * 2 * 4 * 2 * 7
+        println("RESULT" + result.toString())
+
+        val testList = liiist
+
         var keys = arrayListOf(0)
-        while (!keys.all { it==testList.last()}) {
-            println("keys=$keys")
+        while (!keys.all { it == testList.last() }) {
             val newKeys = arrayListOf<Int>()
             for (key in keys) {
-                println(key)
-                if(key == testList.last()){
+                if (key == testList.last()) {
                     newKeys.add(key)
 
-                }else {
+                } else {
 
-                        if (testList.contains(key+1)) {
-                            newKeys.add(key+1)
-                            println("key+1=${key+1}")
-                        }
-                        if (testList.contains(key+2)) {
-                            newKeys.add(key+2)
-                            println("key+2=${key+2}")
-                        }
-                        if (testList.contains(key+3)) {
-                            newKeys.add(key+3)
-                            println("key+2=${key+2}")
-                        }
+                    if (testList.contains(key + 1)) {
+                        newKeys.add(key + 1)
+                    }
+                    if (testList.contains(key + 2)) {
+                        newKeys.add(key + 2)
+                    }
+                    if (testList.contains(key + 3)) {
+                        newKeys.add(key + 3)
+                    }
 
                 }
 
             }
             keys = newKeys
         }
-
-        println("keys" + keys.toString())
-
         println(keys.size)
-
-
-/*        val resultList = arrayListOf<ArrayList<Int>>(arrayListOf(0))
-        for (list in resultList){
-            val item = list.last()
-
-            if (containsDiffOne(testList, item)
-                && !containsDiffTwo(testList, item)
-                && !containsDiffThree(testList, item)
-            ) {
-                val list1 = list.toMutableList()
-                list1.add(item)
-                resultList.add(list1)
-            }
-
-            if (containsDiffTwo(testList, item)
-                && !containsDiffOne(testList, item)
-                && !containsDiffThree(testList, item)
-            ) {
-                listOfPair.add(item to item + 2)
-            }
-
-            if (containsDiffThree(testList, item)
-                && !containsDiffOne(testList, item)
-                && !containsDiffTwo(testList, item)
-            ) {
-                listOfPair.add(item to item + 3)
-            }
-
-        }*/
-
-    }
-
-    private fun filterStraightforwardLists(newList: MutableList<Int>) {
-        val listOfPair = arrayListOf<Pair<Int, Int>>()
-        for (item in newList) {
-            if (containsDiffOne(newList, item)
-                && !containsDiffTwo(newList, item)
-                && !containsDiffThree(newList, item)
-            ) {
-                println("$item -> ${item + 1}")
-                listOfPair.add(item to item + 1)
-            }
-
-            if (containsDiffTwo(newList, item)
-                && !containsDiffOne(newList, item)
-                && !containsDiffThree(newList, item)
-            ) {
-                println("$item -> ${item + 2}")
-                listOfPair.add(item to item + 2)
-            }
-
-            if (containsDiffThree(newList, item)
-                && !containsDiffOne(newList, item)
-                && !containsDiffTwo(newList, item)
-            ) {
-                println("$item -> ${item + 3}")
-                listOfPair.add(item to item + 3)
-            }
-        }
-        println(listOfPair)
-
-        for (pairWithIndex in listOfPair.withIndex()) {
-            if (pairWithIndex.index + 1 < listOfPair.size) {
-                if (listOfPair.get(pairWithIndex.index + 1).first == pairWithIndex.value.second) {
-                    println(
-                        pairWithIndex.value.first.toString() + "->" + pairWithIndex.value.second.toString() + "->" + listOfPair.get(
-                            pairWithIndex.index + 1
-                        ).second
-                    )
-                }
-            }
-        }
-
-        val listOfStraightLines = arrayListOf<ArrayList<Int>>()
-        val pairToMap = listOfPair.toMap()
-        for (item in newList) {
-            if (pairToMap.keys.contains(item)) {
-                var currentSecond = pairToMap.get(item) ?: error("error 2")
-                val list = arrayListOf(item, currentSecond)
-                while (currentSecond in pairToMap.keys) {
-                    currentSecond = pairToMap.get(currentSecond) ?: error("error 3")
-                    list.add(currentSecond)
-                }
-
-                listOfStraightLines.add(list)
-                println(list.toString())
-            }
-
-        }
-
-        val listToTheEnd = listOfStraightLines.filter { it.last() == deviceRate }
-        val resultList = listToTheEnd.toMutableList()
-
-        for (i in 1..listToTheEnd.size - 1) {
-            if (listToTheEnd.get(i).all { listToTheEnd.first().contains(it) } && onlyOneStarter(
-                    listToTheEnd.get(i).first()
-                )) {
-                resultList.remove(listToTheEnd.get(i))
-            }
-        }
-
-        println(resultList.toString())
-        if(resultList.isNotEmpty()){
-            val listEnd = resultList.last().first()
-            val newListWithIndex = newList.mapIndexed { index, i -> i to index }.toMap()
-            val listEndIndex = newListWithIndex.get(listEnd) ?: error("")
-            println(listEndIndex)
-            newList.take(listEndIndex)
-        }
     }
 
     private fun containsDiffOne(newList: MutableList<Int>, item: Int) =
         newList.contains(item + 1)
-
-    private fun containsDiffTwo(newList: MutableList<Int>, item: Int) =
-        newList.contains(item + 2)
-
-    private fun containsDiffThree(newList: MutableList<Int>, item: Int) =
-        newList.contains(item + 3)
-
-    private fun onlyOneStarter(item:Int):Boolean{
-        if (inputs.contains(item-1)&&!inputs.contains(item-2)&&!inputs.contains(item-3)) return true
-        if(!inputs.contains(item-1)&&inputs.contains(item-2)&&!inputs.contains(item-3))return true
-        if(!inputs.contains(item-1)&&!inputs.contains(item-2)&&inputs.contains(item-3))return true
-        return false
-    }
 }
 
 fun main() {
     println(Day10().getSolution1())
+    //1322306994176
     println(Day10().getSolution2())
 }
