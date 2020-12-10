d6edab0dc60d9df86535a8d88255bfd1ba2bdab4
Jinyan Liu <jinyan.liu@bontouch.com>
2020-12-10 21:10:14 +0100

    Refactor day 10

--- aoc2020/src/main/kotlin/D10.kt
+++ aoc2020/src/main/kotlin/D10.kt
@@ -14,7 +14,7 @@ class Day10 {
         var threeJoltDifferenceCounter = 0
 
         while (currentOutput < deviceRate) {
-            if (containsDiffOne(newList, currentOutput)) {
+            if (newList.contains(currentOutput + 1)) {
                 currentOutput += 1
                 oneJoltDifferenceCounter += 1
             } else if (newList.contains(currentOutput + 3)) {
@@ -26,7 +26,7 @@ class Day10 {
         return oneJoltDifferenceCounter * threeJoltDifferenceCounter
     }
 
-    fun getSolution2() {
+    fun getSolution2():Long {
         val newList = inputs.toMutableList()
         newList.add(0)
         newList.add(deviceRate)
@@ -34,109 +34,62 @@ class Day10 {
 
         println("newList=" + newList.toString())
 
-        val liiist = arrayListOf(
-            0,
-            1,
-            2,
-            3,
-            4,
-            7,
-            8,
-            11,
-            12,
-            13,
-            16,
-            17,
-            18,
-            19,
-            20,
-            23,
-            26,
-            27,
-            28,
-            31,
-            34,
-            35,
-            36,
-            37,
-            40,
-            41,
-            42,
-            43,
-            46,
-            49
-        )
-        val originList = arrayListOf(
-            0,
-            1,
-            2,
-            3,
-            4,
-            7,
-            8,
-            11,
-            12,
-            13,
-            16,
-            17,
-            18,
-            19,
-            20,
-            23,
-            26,
-            27,
-            28,
-            31,
-            34,
-            35,
-            36,
-            37,
-            40,
-            41,
-            42,
-            43,
-            46,
-            49
-        )
 
-        val result: Long = 3136L * 7 * 7 * 4 * 7 * 7 * 2 * 7 * 7 * 4 * 2 * 4 * 2 * 7
-        println("RESULT" + result.toString())
+        val multiplyList = arrayListOf<Long>()
+        var toVerifyList = newList.toList()
+        while (toVerifyList.isNotEmpty()) {
+            if (toVerifyList[toVerifyList.size - 1] - toVerifyList[toVerifyList.size - 1 - 4] == 4) {
+                toVerifyList = toVerifyList.dropLast(5)
+                multiplyList.add(7L)
+            } else if (toVerifyList[toVerifyList.size - 1] - toVerifyList[toVerifyList.size - 1 - 3] == 3) {
+                toVerifyList=toVerifyList.dropLast(4)
+                multiplyList.add(4L)
+            } else if (toVerifyList[toVerifyList.size - 1] - toVerifyList[toVerifyList.size - 1 - 2] == 2) {
+                toVerifyList=toVerifyList.dropLast(3)
+                multiplyList.add(2L)
+            } else {
+                toVerifyList=toVerifyList.dropLast(1)
+            }
+        }
+        println(multiplyList.toString())
+        return multiplyList.reduce{ accumulator, element -> accumulator * element }
+    }
 
-        val testList = liiist
+    fun getTestSolution2():Int{
+        val newList = IoHelper.getInts("d10Test2.in").toMutableList()
+        val deviceRate = (newList.max() ?: error("error 1")) + 3
+        newList.add(0)
+        newList.add(deviceRate)
+        newList.sort()
+        println(newList.toString())
 
         var keys = arrayListOf(0)
-        while (!keys.all { it == testList.last() }) {
+        while (!keys.all { it == newList.last() }) {
             val newKeys = arrayListOf<Int>()
             for (key in keys) {
-                if (key == testList.last()) {
+                if (key == newList.last()) {
                     newKeys.add(key)
-
                 } else {
-
-                    if (testList.contains(key + 1)) {
+                    if (newList.contains(key + 1)) {
                         newKeys.add(key + 1)
                     }
-                    if (testList.contains(key + 2)) {
+                    if (newList.contains(key + 2)) {
                         newKeys.add(key + 2)
                     }
-                    if (testList.contains(key + 3)) {
+                    if (newList.contains(key + 3)) {
                         newKeys.add(key + 3)
                     }
-
                 }
 
             }
             keys = newKeys
         }
-        println(keys.size)
+        return keys.size
     }
-
-    private fun containsDiffOne(newList: MutableList<Int>, item: Int) =
-        newList.contains(item + 1)
 }
 
 fun main() {
     println(Day10().getSolution1())
-    //1322306994176
     println(Day10().getSolution2())
+    println(Day10().getTestSolution2())
 }
