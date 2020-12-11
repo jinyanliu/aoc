24e8b319522979476fe9d0a3141fbcf300942e5f
Jinyan Liu <jinyan.liu@bontouch.com>
2020-12-11 23:49:57 +0100

    Prepare day 12

---
+++ aoc2020/src/main/kotlin/D12.kt
@@ -0,0 +1,16 @@
+import utils.IoHelper
+
+class Day12 {
+    private val inputs = IoHelper.getLines("d12.in")
+
+    fun getSolution1() {
+    }
+
+    fun getSolution2() {
+    }
+}
+
+fun main() {
+    println(Day12().getSolution1())
+    println(Day12().getSolution2())
+}
