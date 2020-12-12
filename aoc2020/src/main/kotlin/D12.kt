3e020b196e56cd312a4d9c180e35a8b9c9c766d1
Jinyan Liu <jinyan.liu@bontouch.com>
2020-12-12 13:49:59 +0100

    Prepare day 13

--- aoc2020/src/main/kotlin/D12.kt
+++ aoc2020/src/main/kotlin/D12.kt
@@ -6,80 +6,27 @@ enum class Direction(val key: String) { WEST("W"), EAST("E"), NORTH("N"), SOUTH(
 // NORTH xCount >0, SOUTH xCount <0, EAST yCount >0, WEST yCount <0
 data class Boat(
     var facing: Direction = Direction.EAST,
-    var xCount: Int = 0,
-    var yCount: Int = 0
-)
+    override var xCount: Int = 0,
+    override var yCount: Int = 0
+) : Moveable
 
 // NORTH xCount >0, SOUTH xCount <0, EAST yCount >0, WEST yCount <0
 data class WayPoint(
-    var xCount: Int = 10,
-    var yCount: Int = 1
-)
+    override var xCount: Int = 10,
+    override var yCount: Int = 1
+) : Moveable
 
 interface Moveable {
     var xCount: Int
     var yCount: Int
 }
 
-class Day12 {
-    private val inputs = IoHelper.getLines("d12.in")
-    private val parsedInputs = inputs.map { it[0].toString() to it.drop(1).toInt() }
-
-    fun getSolution1(): Int {
-        val boat = Boat()
-        parsedInputs.forEach {
-            var toVerify = it
-            if (toVerify.first == "R" || toVerify.first == "L") {
-                boat.facing = boat.turnTo(toVerify)
-            }
-            if (toVerify.first == "F") {
-                toVerify = boat.facing.key to toVerify.second
-            }
-            boat.move(toVerify)
-        }
-        return boat.xCount.absoluteValue + boat.yCount.absoluteValue
-    }
-
-    private fun Boat.move(instruction: Pair<String, Int>) {
-        when (instruction.first) {
-            Direction.NORTH.key -> this.yCount = this.yCount + instruction.second
-            Direction.SOUTH.key -> this.yCount = this.yCount - instruction.second
-            Direction.EAST.key -> this.xCount = this.xCount + instruction.second
-            Direction.WEST.key -> this.xCount = this.xCount - instruction.second
-        }
-    }
-
-    private fun WayPoint.move(instruction: Pair<String, Int>) {
-        when (instruction.first) {
-            Direction.NORTH.key -> this.yCount = this.yCount + instruction.second
-            Direction.SOUTH.key -> this.yCount = this.yCount - instruction.second
-            Direction.EAST.key -> this.xCount = this.xCount + instruction.second
-            Direction.WEST.key -> this.xCount = this.xCount - instruction.second
-        }
-    }
-
-    fun getSolution2(): Int {
-        val boat = Boat()
-        val wayPoint = WayPoint()
-
-        parsedInputs.forEach {
-            if (it.first == "F") {
-                boat.xCount = boat.xCount + it.second * wayPoint.xCount
-                boat.yCount = boat.yCount + it.second * wayPoint.yCount
-            }
-            if (it.first == "R" || it.first == "L") {
-                val (xCount, yCount) = wayPoint.turnTo(it)
-                wayPoint.xCount = xCount
-                wayPoint.yCount = yCount
-            }
-            when (it.first) {
-                Direction.NORTH.key -> wayPoint.yCount = wayPoint.yCount + it.second
-                Direction.SOUTH.key -> wayPoint.yCount = wayPoint.yCount - it.second
-                Direction.EAST.key -> wayPoint.xCount = wayPoint.xCount + it.second
-                Direction.WEST.key -> wayPoint.xCount = wayPoint.xCount - it.second
-            }
-        }
-        return boat.xCount.absoluteValue + boat.yCount.absoluteValue
+private fun Moveable.move(instruction: Pair<String, Int>) {
+    when (instruction.first) {
+        Direction.NORTH.key -> this.yCount = this.yCount + instruction.second
+        Direction.SOUTH.key -> this.yCount = this.yCount - instruction.second
+        Direction.EAST.key -> this.xCount = this.xCount + instruction.second
+        Direction.WEST.key -> this.xCount = this.xCount - instruction.second
     }
 }
 
@@ -93,19 +40,56 @@ fun Boat.turnTo(instruction: Pair<String, Int>): Direction = when {
     (this.facing == Direction.SOUTH) && (instruction.second == 90) || (this.facing == Direction.NORTH) && (instruction.second == 270) -> if (instruction.first == "R") Direction.WEST else Direction.EAST
     (this.facing == Direction.EAST) && (instruction.second == 270) || (this.facing == Direction.WEST) && (instruction.second == 90) -> if (instruction.first == "R") Direction.NORTH else Direction.SOUTH
     (this.facing == Direction.SOUTH) && (instruction.second == 270) || (this.facing == Direction.NORTH) && (instruction.second == 90) -> if (instruction.first == "R") Direction.EAST else Direction.WEST
-    else -> error("direction turn right to")
+    else -> error("boat turning error")
 }
 
 fun WayPoint.turnTo(instruction: Pair<String, Int>): Pair<Int, Int> = when {
     (instruction.second == 90 && instruction.first == "R") || (instruction.second == 270 && instruction.first == "L") -> this.yCount to this.xCount * -1
     instruction.second == 180 -> this.xCount * -1 to this.yCount * -1
     (instruction.second == 270 && instruction.first == "R") || (instruction.second == 90 && instruction.first == "L") -> this.yCount * -1 to this.xCount
-    else -> error("way point ture right to")
+    else -> error("way point turning error")
+}
+
+class Day12 {
+    private val inputs = IoHelper.getLines("d12.in")
+    private val parsedInputs = inputs.map { it[0].toString() to it.drop(1).toInt() }
+
+    fun getSolution1(): Int {
+        val boat = Boat()
+        parsedInputs.forEach {
+            if (it.first == "R" || it.first == "L") {
+                boat.facing = boat.turnTo(it)
+            }
+            var toVerify = it
+            if (toVerify.first == "F") {
+                toVerify = boat.facing.key to toVerify.second
+            }
+            boat.move(toVerify)
+        }
+        return boat.xCount.absoluteValue + boat.yCount.absoluteValue
+    }
+
+    fun getSolution2(): Int {
+        val boat = Boat()
+        val wayPoint = WayPoint()
+
+        parsedInputs.forEach {
+            if (it.first == "R" || it.first == "L") {
+                val (xCount, yCount) = wayPoint.turnTo(it)
+                wayPoint.xCount = xCount
+                wayPoint.yCount = yCount
+            }
+            if (it.first == "F") {
+                boat.xCount = boat.xCount + it.second * wayPoint.xCount
+                boat.yCount = boat.yCount + it.second * wayPoint.yCount
+            }
+            wayPoint.move(it)
+        }
+        return boat.xCount.absoluteValue + boat.yCount.absoluteValue
+    }
 }
 
 fun main() {
-    //362
     println(Day12().getSolution1())
-    //29895
     println(Day12().getSolution2())
 }
---
+++ aoc2020/src/main/kotlin/D13.kt
@@ -0,0 +1,16 @@
+import utils.IoHelper
+
+class Day13 {
+    private val inputs = IoHelper.getLines("d13.in")
+
+    fun getSolution1() {
+    }
+
+    fun getSolution2() {
+    }
+}
+
+fun main() {
+    println(Day13().getSolution1())
+    println(Day13().getSolution2())
+}
