3c1ae1eaa2bdd5d47939be5922c551b25db23fd9
Jinyan Liu <jinyan.liu@bontouch.com>
2020-12-12 11:43:51 +0100

    Finish day 12 task 1

--- aoc2020/src/main/kotlin/D12.kt
+++ aoc2020/src/main/kotlin/D12.kt
@@ -1,16 +1,128 @@
 import utils.IoHelper
+import kotlin.math.absoluteValue
+
+enum class Direction(val key: String) { WEST("W"), EAST("E"), NORTH("N"), SOUTH("S") }
+enum class XDirection(val key: String) { WEST("W"), EAST("E") }
+enum class YDirection(val key: String) { NORTH("N"), SOUTH("S") }
 
 class Day12 {
     private val inputs = IoHelper.getLines("d12.in")
+    private val parsedInputs = inputs.map { it.get(0).toString() to it.drop(1).toInt() }
 
-    fun getSolution1() {
+    fun getSolution1(): Int {
+        val boat = Boat()
+        println(parsedInputs)
+        parsedInputs.forEach {
+            var toVerify = it
+            if (toVerify.first == "F") {
+                toVerify = boat.facing.key to toVerify.second
+            }
+            when (toVerify.first) {
+                Direction.NORTH.key -> boat.yCount = boat.yCount + toVerify.second
+                Direction.SOUTH.key -> boat.yCount = boat.yCount - toVerify.second
+                Direction.EAST.key -> boat.xCount = boat.xCount + toVerify.second
+                Direction.WEST.key -> boat.xCount = boat.xCount - toVerify.second
+            }
+            if (toVerify.first == "R") {
+                boat.facing = boat.facing.turnRightTo(toVerify.second)
+            }
+            if (toVerify.first == "L") {
+                boat.facing = boat.facing.turnLeftTo(toVerify.second)
+            }
+        }
+
+        println("xCount=" + boat.xCount + "yCount=" + boat.yCount)
+        return boat.xCount.absoluteValue + boat.yCount.absoluteValue
     }
 
     fun getSolution2() {
     }
 }
 
+data class Boat(
+    var facing: Direction = Direction.EAST,
+    var xDirection: XDirection = XDirection.EAST,
+    var xCount: Int = 0,
+    var yDirection: YDirection = YDirection.NORTH,
+    var yCount: Int = 0
+)
+
+fun Direction.turnRightTo(degree: Int): Direction {
+    when (this) {
+        Direction.EAST -> {
+            return when (degree) {
+                90 -> Direction.SOUTH
+                180 -> Direction.WEST
+                270 -> Direction.NORTH
+                else -> error("")
+            }
+        }
+        Direction.SOUTH -> {
+            return when (degree) {
+                90 -> Direction.WEST
+                180 -> Direction.NORTH
+                270 -> Direction.EAST
+                else -> error("")
+            }
+        }
+        Direction.WEST -> {
+            return when (degree) {
+                90 -> Direction.NORTH
+                180 -> Direction.EAST
+                270 -> Direction.SOUTH
+                else -> error("")
+            }
+        }
+        Direction.NORTH -> {
+            return when (degree) {
+                90 -> Direction.EAST
+                180 -> Direction.SOUTH
+                270 -> Direction.WEST
+                else -> error("")
+            }
+        }
+    }
+}
+
+fun Direction.turnLeftTo(degree: Int): Direction {
+    when (this) {
+        Direction.EAST -> {
+            return when (degree) {
+                90 -> Direction.NORTH
+                180 -> Direction.WEST
+                270 -> Direction.SOUTH
+                else -> error("")
+            }
+        }
+        Direction.SOUTH -> {
+            return when (degree) {
+                90 -> Direction.EAST
+                180 -> Direction.NORTH
+                270 -> Direction.WEST
+                else -> error("")
+            }
+        }
+        Direction.WEST -> {
+            return when (degree) {
+                90 -> Direction.SOUTH
+                180 -> Direction.EAST
+                270 -> Direction.NORTH
+                else -> error("")
+            }
+        }
+        Direction.NORTH -> {
+            return when (degree) {
+                90 -> Direction.WEST
+                180 -> Direction.SOUTH
+                270 -> Direction.EAST
+                else -> error("")
+            }
+        }
+    }
+}
+
 fun main() {
+    //362
     println(Day12().getSolution1())
     println(Day12().getSolution2())
 }
