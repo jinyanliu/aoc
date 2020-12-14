718ee3d71df028b5b595c33638ca75a25fb8a016
Jinyan Liu <jinyan.liu@bontouch.com>
2020-12-14 22:48:03 +0100

    Finish day 14 Solution2 Alt

---
+++ aoc2020/src/main/kotlin/D15.kt
@@ -0,0 +1,16 @@
+import utils.IoHelper
+
+class Day15 {
+    private val inputs = IoHelper.getLines("d15.in")
+
+    fun getSolution1() {
+    }
+
+    fun getSolution2() {
+    }
+}
+
+fun main() {
+    println(Day15().getSolution1())
+    println(Day15().getSolution2())
+}
