f9ad5f2c4b08203978753f6ce857b7974e7c12c6
Jinyan Liu <jinyan.liu@bontouch.com>
2020-12-16 23:14:32 +0100

    Prepare day 17

---
+++ aoc2020/src/main/kotlin/D17.kt
@@ -0,0 +1,16 @@
+import utils.IoHelper
+
+class Day17 {
+    private val inputs = IoHelper.getLines("d17.in")
+
+    fun getSolution1() {
+    }
+
+    fun getSolution2() {
+    }
+}
+
+fun main() {
+    println(Day17().getSolution1())
+    println(Day17().getSolution2())
+}
