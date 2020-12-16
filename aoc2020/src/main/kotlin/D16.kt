c0b3216b251af3eae326e1329ca307ee4b5533d1
Jinyan Liu <jinyan.liu@bontouch.com>
2020-12-16 22:21:44 +0100

    Refactor day 16

--- aoc2020/src/main/kotlin/D16.kt
+++ aoc2020/src/main/kotlin/D16.kt
@@ -5,31 +5,28 @@ class Day16 {
     private val ticketFields = inputs[0]
     private val myTicket = inputs[1]
     private val nearbyTickets = inputs[2]
+
     private val flatRules = ticketFields.lines().map { it.split(": ")[1] }.flatMap { it.split(" or ") }
         .map { it.split("-")[0].toInt()..it.split("-")[1].toInt() }
     private val rules = ticketFields.lines().map { it.split(": ")[1] }
         .map { ruleValue -> ruleValue.split(" or ").map { it.split("-")[0].toInt()..it.split("-")[1].toInt() } }
+
     private val flatNearbyInts = nearbyTickets.lines().drop(1).flatMap { it.split(",") }.map { it.toInt() }
     private val inValidInts = flatNearbyInts.filter { !(flatRules.any { singleRule -> it in singleRule }) }
+    private val validNearbyTickets = nearbyTickets.lines().drop(1)
+        .map { nearbyTicket -> nearbyTicket.split(",").map { it.toInt() } }
+        .filter { intsList -> intsList.all { it !in inValidInts } }
 
     fun getSolution1() = inValidInts.sum()
 
     fun getSolution2(): Long {
-        val validNearbyTickets = nearbyTickets.lines().drop(1)
-            .map { nearbyTicket ->
-                nearbyTicket.split(",")
-                    .map { it.toInt() }
-            }
-            .filter { it.all { it !in inValidInts } }
         val indexMap = mutableMapOf<Int, ArrayList<Int>>()
-        for (i in rules.indices) {
-            indexMap[i] = arrayListOf()
-        }
-
+        rules.forEachIndexed { index, _ -> indexMap[index] = arrayListOf() }
         validNearbyTickets.forEach { ints ->
             ints.withIndex().forEach { indexMap[it.index]?.add(it.value) }
         }
 
+        val ruleIndexMap = mutableMapOf<List<IntRange>, Int>()
         val leftOverRules = rules.toMutableList()
         while (leftOverRules.size > 0) {
             indexMap.forEach { mapElement ->
@@ -39,12 +36,14 @@ class Day16 {
                 }
                 if (newRules.size == 1) {
                     leftOverRules.remove(newRules[0])
+                    ruleIndexMap[newRules[0]] = mapElement.key
                 }
             }
         }
 
-        val myTicket = myTicket.lines()[1].split(",").map { it.toInt() }
-        return myTicket.withIndex().filter { it.index in arrayListOf(16, 2, 1, 5, 17, 13) }.map { it.value.toLong() }
+        val resultIndexToTake = ruleIndexMap.filter { it.key in rules.take(6) }.map { it.value }
+        val myTicket = myTicket.lines()[1].split(",").map { it.toLong() }
+        return myTicket.withIndex().filter { it.index in resultIndexToTake }.map { it.value }
             .reduce { a, b -> a * b }
     }
 }
