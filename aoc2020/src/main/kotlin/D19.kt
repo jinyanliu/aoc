c3be010794fcffb40ccbfe4c20003085d504d94e
Jinyan Liu <jinyan.liu@bontouch.com>
2020-12-19 12:17:13 +0100

    Stage

--- aoc2020/src/main/kotlin/D19.kt
+++ aoc2020/src/main/kotlin/D19.kt
@@ -1,9 +1,57 @@
 import utils.IoHelper
 
 class Day19 {
-    private val inputs = IoHelper.getLines("d19.in")
+    private val inputs = IoHelper.getSections("d19Test.in")
+    private val rules = inputs[0].lines()
+    private val messages = inputs[1].lines()
+
+    private fun inputsMap(): Map<Long, String> {
+        val inputsMap = mutableMapOf<Long, String>()
+        rules.map { inputsMap[it.split(": ")[0].toLong()] = it.split(": ")[1] }
+        return inputsMap
+    }
+
 
     fun getSolution1() {
+/*        println(inputsMap())
+        val zeroRule = inputsMap()[0]!!
+        var possibleRules = arrayListOf<String>()
+        possibleRules.addAll(zeroRule.split(" "))
+        println(possibleRules)
+
+        while (true){
+            val newToCheck = arrayListOf<String>()
+
+            for (possibleRule in possibleRules) {
+                val innerRules = inputsMap().get(possibleRule.toLong())!!
+                if(innerRules.startsWith("\"")){
+                    newToCheck.add(innerRules)
+                }else if(innerRules.contains("|")){
+                    if(newToCheck.isNotEmpty()){
+
+                    }
+                }else {
+
+                }
+            }
+        }*/
+
+
+        var toCheck = arrayListOf<String>("0")
+        while (toCheck.any { it.any { it.toString().toLongOrNull()!= null } } ){
+            val newList = arrayListOf<String>()
+            toCheck.forEach {
+                if(it.toLongOrNull()!= null){
+                    val values = inputsMap().get(it.toLong())!!
+                    if(!values.contains("|")){
+                        newList.add(values)
+                    }
+                }
+            }
+            toCheck = newList
+            println(toCheck)
+        }
+
     }
 
     fun getSolution2() {
