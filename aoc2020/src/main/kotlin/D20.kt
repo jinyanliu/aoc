3d008e29f77d340022903f4a68f985774dc60fa3
Jinyan Liu <jinyan.liu@bontouch.com>
2020-12-19 21:54:54 +0100

    Prepare day 20

---
+++ aoc2020/src/main/kotlin/D20.kt
@@ -0,0 +1,16 @@
+import utils.IoHelper
+
+class Day20 {
+    private val inputs = IoHelper.getLines("d20.in")
+
+    fun getSolution1() {
+    }
+
+    fun getSolution2() {
+    }
+}
+
+fun main() {
+    println(Day20().getSolution1())
+    println(Day20().getSolution2())
+}
