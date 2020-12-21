41cfb576789d002a36533a1dfdc654cdc9c6b89f
Jinyan Liu <jinyan.liu@bontouch.com>
2020-12-21 11:20:38 +0100

    Finish day 21 part 2

--- aoc2020/src/main/kotlin/D21.kt
+++ aoc2020/src/main/kotlin/D21.kt
@@ -3,7 +3,7 @@ import utils.IoHelper
 class Day21 {
     private val inputs = IoHelper.getLines("d21.in")
 
-    fun getSolution1() {
+    fun getSolution1(): Int {
         var inputsPairList = inputs.map {
             val parts = it.dropLast(1).split(" (contains ")
             val ingredientsList = parts[0].split(" ").toMutableList()
@@ -29,34 +29,14 @@ class Day21 {
 
             println("currentIntersect=" + currentIntersect)
 
-/*            if (currentIntersect.size == 1) {
-                println("currentIntersect == 1")
-                println("currentIntersect="+currentIntersect.toMutableList()[0])
-                println("allergen="+allergen)
-                inputsPairList = inputsPairList.map {
-                    val ingredientsList = it.first
-                    ingredientsList.remove(currentIntersect.toMutableList()[0])
-                    val allergenList = it.second
-                    allergenList.remove(allergen)
-                    ingredientsList to allergenList
-                }
-            }else {
-                println("currentIntersect == "+currentIntersect.size)
-                println("currentIntersect="+currentIntersect.toMutableList())
-                println("allergen="+allergen)
-            }*/
+            inputsPairList = inputsPairList.map {
+                val ingredientsList = it.first
+                ingredientsList.removeAll(currentIntersect.toMutableList())
+                val allergenList = it.second
+                allergenList.remove(allergen)
+                ingredientsList to allergenList
+            }
 
-
-
-                println("currentIntersect="+currentIntersect.toMutableList())
-                println("allergen="+allergen)
-                inputsPairList = inputsPairList.map {
-                    val ingredientsList = it.first
-                    ingredientsList.removeAll(currentIntersect.toMutableList())
-                    val allergenList = it.second
-                    allergenList.remove(allergen)
-                    ingredientsList to allergenList
-                }
 
 
             println()
@@ -64,14 +44,66 @@ class Day21 {
             println()
         }
 
-        println(inputsPairList.map { it.first.count() }.sum())
+        return inputsPairList.map { it.first.count() }.sum()
     }
 
-    fun getSolution2() {
+    fun getSolution2():String {
+        var inputsPairList = inputs.map {
+            val parts = it.dropLast(1).split(" (contains ")
+            val ingredientsList = parts[0].split(" ").toMutableList()
+            val allergensList = parts[1].split(", ").toMutableList()
+            ingredientsList to allergensList
+        }
+        println(inputsPairList)
+
+        val result = arrayListOf<Pair<String, String>>()
+
+        while (inputsPairList.flatMap { it.second }.isNotEmpty()) {
+
+            val allAllergens = inputsPairList.flatMap { it.second }.toSet()
+            println(allAllergens)
+
+            for (allergen in allAllergens) {
+                val currentAllergenList = inputsPairList.filter { it.second.contains(allergen) }
+                println("currentAllergenList=" + currentAllergenList)
+
+                val ingredientsList = currentAllergenList.map { it.first }
+                println("ingredientsList=" + ingredientsList)
+
+                var currentIntersect = ingredientsList[0].toSet()
+
+                for (i in 0..ingredientsList.size - 1) {
+                    currentIntersect = ingredientsList[i].intersect(currentIntersect)
+                }
+
+                println("currentIntersect=" + currentIntersect)
+
+                if (currentIntersect.size == 1) {
+                    inputsPairList = inputsPairList.map {
+                        val ingredientsList = it.first
+                        ingredientsList.removeAll(currentIntersect.toMutableList())
+                        val allergenList = it.second
+                        allergenList.remove(allergen)
+                        ingredientsList to allergenList
+                    }
+
+                    result.add(currentIntersect.toMutableList()[0] to allergen)
+                }
+            }
+        }
+
+        result.sortBy { it.second }
+
+        println(inputsPairList.map { it.first.count() }.sum())
+
+        return result.map { it.first }.joinToString(",")
+
     }
 }
 
 fun main() {
+    // 2203
     println(Day21().getSolution1())
+    // fqfm,kxjttzg,ldm,mnzbc,zjmdst,ndvrq,fkjmz,kjkrm
     println(Day21().getSolution2())
 }
