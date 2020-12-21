d9f6d2b7bcf2aa77ffb061b186ae4ac3cf80d976
Jinyan Liu <jinyan.liu@bontouch.com>
2020-12-21 10:38:17 +0100

    Stage

--- aoc2020/src/main/kotlin/D21.kt
+++ aoc2020/src/main/kotlin/D21.kt
@@ -1,9 +1,43 @@
 import utils.IoHelper
 
 class Day21 {
-    private val inputs = IoHelper.getLines("d21.in")
+    private val inputs = IoHelper.getLines("d21Test.in")
 
     fun getSolution1() {
+        val inputsPairList = inputs.map {
+            val parts = it.dropLast(1).split(" (contains ")
+            val ingredientsList = parts[0].split(" ").toMutableList()
+            val allergensList = parts[1].split(", ").toMutableList()
+            ingredientsList to allergensList
+        }
+        println(inputsPairList)
+        val allAllergens = inputsPairList.flatMap { it.second }.toSet()
+        println(allAllergens)
+
+        for(allergen in allAllergens){
+            val currentAllergenList = inputsPairList.filter { it.second.contains(allergen)}
+            println("currentAllergenList"+currentAllergenList)
+
+            val ingredientsList = currentAllergenList.map { it.first }
+            println("ingredientsList"+ingredientsList)
+
+            var currentIntersect = ingredientsList[0].toSet()
+
+            for(i in 0..ingredientsList.size-1){
+                currentIntersect= ingredientsList[i].intersect(currentIntersect)
+            }
+
+            println("currentIntersect"+currentIntersect)
+
+            if(currentIntersect.size == 1){
+                println(currentIntersect)
+                println(allergen)
+            }
+
+            println()
+            println()
+            println()
+        }
     }
 
     fun getSolution2() {
