9f84e402cbd514bb9130250ec37c36df8b04f64a
Jinyan Liu <jinyan.liu@bontouch.com>
2020-12-22 14:29:03 +0100

    Finish day 22

--- aoc2020/src/main/kotlin/D22.kt
+++ aoc2020/src/main/kotlin/D22.kt
@@ -5,14 +5,12 @@ enum class PLAYER {
 }
 
 class Day22 {
-    private val inputs = IoHelper.getSections("d22Test.in")
+    private val inputs = IoHelper.getSections("d22.in")
 
     fun getSolution1(): Long {
         val playerOne = inputs[0].lines().drop(1).map { it.toLong() }.toMutableList()
         val playerTwo = inputs[1].lines().drop(1).map { it.toLong() }.toMutableList()
-
         val winner = playGame(playerOne, playerTwo)
-
         return if (winner == PLAYER.PLAYER_ONE) {
             playerOne.mapIndexed { index, l -> l * (playerOne.size - index) }.sum()
         } else {
@@ -24,7 +22,7 @@ class Day22 {
         val playerOne = inputs[0].lines().drop(1).map { it.toLong() }.toMutableList()
         val playerTwo = inputs[1].lines().drop(1).map { it.toLong() }.toMutableList()
 
-        val winner = playRecursiveGame(playerOne, playerTwo)
+        val winner = playRecursiveGame(playerOne, playerTwo, arrayListOf(playerOne.toList() to playerTwo.toList()))
 
         return if (winner == PLAYER.PLAYER_ONE) {
             playerOne.mapIndexed { index, l -> l * (playerOne.size - index) }.sum()
@@ -56,7 +54,8 @@ class Day22 {
 
     private fun playRecursiveGame(
         playerOne: MutableList<Long>,
-        playerTwo: MutableList<Long>
+        playerTwo: MutableList<Long>,
+        gameHistory: ArrayList<Pair<List<Long>, List<Long>>>
     ): PLAYER {
         while (playerOne.isNotEmpty() && playerTwo.isNotEmpty()) {
             val playerOneFirst = playerOne.first()
@@ -65,7 +64,11 @@ class Day22 {
             playerTwo.removeAt(0)
 
             if (playerOne.size.toLong() >= playerOneFirst && playerTwo.size.toLong() >= playerTwoFirst) {
-                val winner = playRecursiveGame(playerOne.toMutableList(), playerTwo.toMutableList())
+                val winner = playRecursiveGame(
+                    playerOne.subList(0, playerOneFirst.toInt()).toList().toMutableList(),
+                    playerTwo.subList(0, playerTwoFirst.toInt()).toList().toMutableList(),
+                    arrayListOf(playerOne.toList() to playerTwo.toList())
+                )
                 if (winner == PLAYER.PLAYER_ONE) {
                     playerOne.add(playerOneFirst)
                     playerOne.add(playerTwoFirst)
@@ -73,7 +76,6 @@ class Day22 {
                     playerTwo.add(playerTwoFirst)
                     playerTwo.add(playerOneFirst)
                 }
-
             } else {
                 if (playerOneFirst > playerTwoFirst) {
                     playerOne.add(playerOneFirst)
@@ -83,6 +85,8 @@ class Day22 {
                     playerTwo.add(playerOneFirst)
                 }
             }
+            if (playerOne to playerTwo in gameHistory) return PLAYER.PLAYER_ONE
+            gameHistory.add(playerOne.toList() to playerTwo.toList())
         }
         return if (playerOne.isNotEmpty()) PLAYER.PLAYER_ONE else PLAYER.PLAYER_TWO
     }
@@ -91,5 +95,6 @@ class Day22 {
 fun main() {
     //32102
     println(Day22().getSolution1())
+    //34173
     println(Day22().getSolution2())
 }
