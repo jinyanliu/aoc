4c79f92289ef131d4137f1507d75ba5324fc9ce7
Jinyan Liu <jinyan.liu@bontouch.com>
2020-12-22 10:25:52 +0100

    Finish day 22 part 1

--- aoc2020/src/main/kotlin/D22.kt
+++ aoc2020/src/main/kotlin/D22.kt
@@ -1,14 +1,33 @@
 import utils.IoHelper
 
+enum class PLAYER {
+    PLAYER_ONE, PLAYER_TWO
+}
+
 class Day22 {
     private val inputs = IoHelper.getSections("d22.in")
 
     fun getSolution1(): Long {
         val playerOne = inputs[0].lines().drop(1).map { it.toLong() }.toMutableList()
         val playerTwo = inputs[1].lines().drop(1).map { it.toLong() }.toMutableList()
-        println(playerOne)
-        println(playerTwo)
 
+        val winner = playSubGame(playerOne, playerTwo)
+
+        return if (winner == PLAYER.PLAYER_ONE) {
+            playerOne.mapIndexed { index, l -> l * (playerOne.size - index) }.sum()
+        } else {
+            playerTwo.mapIndexed { index, l -> l * (playerTwo.size - index) }.sum()
+        }
+    }
+
+    fun getSolution2() {
+
+    }
+
+    private fun playSubGame(
+        playerOne: MutableList<Long>,
+        playerTwo: MutableList<Long>
+    ): PLAYER {
         while (playerOne.isNotEmpty() && playerTwo.isNotEmpty()) {
             val playerOneFirst = playerOne.first()
             val playerTwoFirst = playerTwo.first()
@@ -18,22 +37,12 @@ class Day22 {
             if (playerOneFirst > playerTwoFirst) {
                 playerOne.add(playerOneFirst)
                 playerOne.add(playerTwoFirst)
-
             } else {
                 playerTwo.add(playerTwoFirst)
                 playerTwo.add(playerOneFirst)
             }
-
         }
-
-        return if (playerOne.isNotEmpty()) {
-            playerOne.mapIndexed { index, l -> l * (playerOne.size - index) }.sum()
-        } else {
-            playerTwo.mapIndexed { index, l -> l * (playerTwo.size - index) }.sum()
-        }
-    }
-
-    fun getSolution2() {
+        return if (playerOne.isNotEmpty()) PLAYER.PLAYER_ONE else PLAYER.PLAYER_TWO
     }
 }
 
