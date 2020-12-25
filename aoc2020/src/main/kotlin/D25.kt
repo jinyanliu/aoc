bf2e541a3faf3efa05db1fc69d6f3992d75d39cd
Jinyan Liu <jinyan.liu@bontouch.com>
2020-12-25 12:33:52 +0100

    Finish day 25 part 1

--- aoc2020/src/main/kotlin/D25.kt
+++ aoc2020/src/main/kotlin/D25.kt
@@ -1,9 +1,42 @@
 import utils.IoHelper
 
 class Day25 {
-    private val inputs = IoHelper.getLines("d25.in")
+    private val inputs = IoHelper.getLines("d25Test.in").map { it.toLong() }
 
     fun getSolution1() {
+        //card
+        val subjectNumber = 7L
+        var value = 1L
+        var loopSizeCard = 0
+        while(value!= 9033205L){
+            loopSizeCard+=1
+            value = value * subjectNumber
+            value = value % 20201227
+        }
+        println(loopSizeCard)
+
+
+        //door
+        val subjectNumber2 = 7L
+        var value2 = 1L
+        var loopSizeDoor = 0
+        while(value2!= 9281649L){
+            loopSizeDoor+=1
+            value2 = value2 * subjectNumber2
+            value2 = value2 % 20201227
+        }
+        println(loopSizeDoor)
+
+
+        val subjectNumberCard2 = 9033205L
+        var valueCard2 = 1L
+        repeat(loopSizeDoor){
+            valueCard2 = valueCard2 * subjectNumberCard2
+            valueCard2 = valueCard2 % 20201227
+        }
+        println(valueCard2)
+
+
     }
 
     fun getSolution2() {
