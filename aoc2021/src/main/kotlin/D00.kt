08c6f3da382fa9042b1fcf4c05ed268a0641652b
jinyanliu <jane.jinyan.liu@gmail.com>
2021-11-30 18:51:43 +0100

    D00

---
+++ aoc2021/src/main/kotlin/D00.kt
@@ -0,0 +1,17 @@
+import utils.IoHelper
+
+class Day00 {
+    private val inputs = IoHelper.getLines("d00.in")
+
+    fun getSolution1(): String {
+        return inputs.joinToString()
+    }
+
+    fun getSolution2() {
+    }
+}
+
+fun main() {
+    println(Day00().getSolution1())
+    println(Day00().getSolution2())
+}
---
+++ aoc2021/src/main/kotlin/utils/IoHelper.kt
@@ -0,0 +1,25 @@
+package utils
+
+class IoHelper {
+    companion object {
+        fun getInts(filename: String): List<Int> {
+            return getLines(filename).map { it.toInt() }
+        }
+
+        fun getLongs(filename: String): List<Long> {
+            return getLines(filename).map { it.toLong() }
+        }
+
+        fun getLines(filename: String): List<String> {
+            return getRawContent(filename).lines()
+        }
+
+        fun getSections(filename: String): List<String> {
+            return getRawContent(filename).split("\n\n")
+        }
+
+        fun getRawContent(filename: String): String {
+            return IoHelper.javaClass.classLoader.getResource(filename)?.readText()?.trim().orEmpty()
+        }
+    }
+}
