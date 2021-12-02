286ee04b168bab85ac80080e54b0afb5e8722fd7
jinyanliu <jane.jinyan.liu@gmail.com>
2021-12-02 09:12:49 +0100

    D02

---
+++ aoc2021/src/main/kotlin/D02.kt
@@ -0,0 +1,79 @@
+import utils.IoHelper
+
+object D02 {
+    private val inputs = IoHelper.getLines("d02.in")
+
+    fun solveOne(): Int {
+        val forwardList = mutableListOf<Int>()
+        val upList = mutableListOf<Int>()
+        val downList = mutableListOf<Int>()
+
+        solve(
+            forwardAction = { value ->
+                forwardList.add(value)
+            },
+            upAction = { value ->
+                upList.add(value)
+            },
+            downAction = { value ->
+                downList.add(value)
+            }
+        )
+
+        val horizontal = forwardList.sum()
+        val vertical = downList.sum() - upList.sum()
+
+        return horizontal * vertical
+    }
+
+    fun solveTwo(): Int {
+        var depth = 0
+        var aim = 0
+
+        val forwardList = mutableListOf<Int>()
+
+        solve(
+            forwardAction = { value ->
+                forwardList.add(value)
+                depth += (aim * value)
+            },
+            upAction = { value ->
+                aim -= value
+            },
+            downAction = { value ->
+                aim += value
+            }
+        )
+
+        val horizontal = forwardList.sum()
+        val vertical = depth
+
+        return horizontal * vertical
+    }
+
+    private fun solve(forwardAction: (Int) -> Unit, upAction: (Int) -> Unit, downAction: (Int) -> Unit) {
+        inputs.forEach {
+            val instructions = it.split(" ")
+            val direction = instructions[0]
+            val value = instructions[1].toInt()
+            when (direction) {
+                "forward" -> {
+                    forwardAction(value)
+                }
+                "up" -> {
+                    upAction(value)
+                }
+                "down" -> {
+                    downAction(value)
+                }
+            }
+        }
+    }
+}
+
+fun main() {
+    val solutionOne = D02.solveOne()
+    val solutionTwo = D02.solveTwo()
+    println("One=$solutionOne")
+    println("Tw0=$solutionTwo")
+}
